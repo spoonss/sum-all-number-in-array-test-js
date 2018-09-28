8aad5ad14608ee04a13c142561d89c4e4ef0543c
Charles Wong <nok.wong@gmail.com>
2018-09-28 01:14:38 +0000

    add recursive function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+
+  let sum = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sum = sum + sumItems(item);
+    } else {  
+      sum = sum + item;
+    }
+  }
+  return sum;
 }
 
-module.exports = sumItems;
+
+  module.exports = sumItems;
+
+
+
